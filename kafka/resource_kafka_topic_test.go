dc82d37928c52208ce31f90847fa1b0bcaa01d64
Sahil Kang <sahil.kang@asilaycomputing.com>
2020-10-20 17:58:04 -0700

    Add acc tests for in-place rf update
    Overall flow:
      * produce a well-known set of messages to a new topic
      * change that topic's rf and partition count
      * verify the changes went through and that no messages were lost

--- kafka/resource_kafka_topic_test.go
+++ kafka/resource_kafka_topic_test.go
@@ -2,11 +2,16 @@ package kafka
 
 import (
 	"fmt"
+	"log"
 	"testing"
+	"time"
+	"strconv"
 
 	uuid "github.com/hashicorp/go-uuid"
 	r "github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+
+	"github.com/Shopify/sarama"
 )
 
 func TestAcc_BasicTopic(t *testing.T) {
@@ -106,6 +111,66 @@ func TestAcc_TopicUpdatePartitions(t *testing.T) {
 	})
 }
 
+func TestAcc_TopicAlterReplicationFactor(t *testing.T) {
+	u, err := uuid.GenerateUUID()
+	if err != nil {
+		t.Fatal(err)
+	}
+	topicName := fmt.Sprintf("syslog-%s", u)
+	bs := testBootstrapServers[0]
+
+	keyEncoder := sarama.StringEncoder("same key -> same partition -> same ordering")
+	messages := []*sarama.ProducerMessage{
+		&sarama.ProducerMessage{
+			Topic: topicName,
+			Key: keyEncoder,
+			Value: sarama.StringEncoder("Krusty"),
+		},
+		&sarama.ProducerMessage{
+			Topic: topicName,
+			Key: keyEncoder,
+			Value: sarama.StringEncoder("Krab"),
+		},
+		&sarama.ProducerMessage{
+			Topic: topicName,
+			Key: keyEncoder,
+			Value: sarama.StringEncoder("Pizza"),
+		},
+	}
+
+	r.Test(t, r.TestCase{
+		Providers: accProvider(),
+		PreCheck: func() { testAccPreCheck(t) },
+		CheckDestroy: testAccCheckTopicDestroy,
+		Steps: []r.TestStep{
+			{
+				Config: fmt.Sprintf(testResourceTopic_updateRF, bs, topicName, 1, 7),
+				Check: r.ComposeTestCheckFunc(
+					testResourceTopic_produceMessages(messages),
+					testResourceTopic_initialCheck),
+			},
+			{
+				Config: fmt.Sprintf(testResourceTopic_updateRF, bs, topicName, 3, 7),
+				Check: r.ComposeTestCheckFunc(
+					testResourceTopic_updateRFCheck,
+					testResourceTopic_checkSameMessages(messages)),
+			},
+			{
+				Config: fmt.Sprintf(testResourceTopic_updateRF, bs, topicName, 2, 8),
+				Check: r.ComposeTestCheckFunc(
+					testResourceTopic_updateRFCheck,
+					testResourceTopic_checkSameMessages(messages)),
+			},
+			{
+				Config: fmt.Sprintf(testResourceTopic_updateRF, bs, topicName, 1, 10),
+				Check: r.ComposeTestCheckFunc(
+					testResourceTopic_updateRFCheck,
+					testResourceTopic_checkSameMessages(messages)),
+			},
+		},
+	})
+}
+
 func testResourceTopic_noConfigCheck(s *terraform.State) error {
 	resourceState := s.Modules[0].Resources["kafka_topic.test"]
 	if resourceState == nil {
@@ -174,6 +239,36 @@ func testResourceTopic_initialCheck(s *terraform.State) error {
 	return nil
 }
 
+func testResourceTopic_produceMessages(messages []*sarama.ProducerMessage) r.TestCheckFunc {
+	return func(s *terraform.State) error {
+		config := testProvider.Meta().(*LazyClient).inner.config
+		kafkaConfig, err := config.newKafkaConfig()
+		if err != nil {
+			return err
+		}
+		kafkaConfig.Producer.Return.Errors = true
+		kafkaConfig.Producer.Return.Successes = true
+		kafkaConfig.Producer.RequiredAcks = sarama.WaitForAll
+
+		producer, err := sarama.NewSyncProducer(*config.BootstrapServers, kafkaConfig)
+		if err != nil {
+			return err
+		}
+
+		defer func() {
+			if err := producer.Close(); err != nil {
+				log.Fatalln(err)
+			}
+		}()
+
+		if err := producer.SendMessages(messages); err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
 func testResourceTopic_updateCheck(s *terraform.State) error {
 	resourceState := s.Modules[0].Resources["kafka_topic.test"]
 	instanceState := resourceState.Primary
@@ -219,6 +314,136 @@ func testResourceTopic_updatePartitionsCheck(s *terraform.State) error {
 	if v, ok := topic.Config["segment.ms"]; ok && *v != "33333" {
 		return fmt.Errorf("segment.ms !=  %v", topic)
 	}
+	return nil
+}
+
+func testResourceTopic_updateRFCheck(s *terraform.State) error {
+	resourceState := s.Modules[0].Resources["kafka_topic.test"]
+	instanceState := resourceState.Primary
+	client := testProvider.Meta().(*LazyClient)
+	topicName := instanceState.Attributes["name"]
+
+	parsed, err := strconv.ParseInt(instanceState.Attributes["replication_factor"], 10, 16)
+	if err != nil {
+		return err
+	}
+	expectedRF := int16(parsed)
+
+	parsed, err = strconv.ParseInt(instanceState.Attributes["partitions"], 10, 32)
+	if err != nil {
+		return err
+	}
+	expectedPartitions := int32(parsed)
+
+	topic, err := client.ReadTopic(topicName)
+	if err != nil {
+		return err
+	}
+
+	if actual := topic.ReplicationFactor; actual != expectedRF {
+		return fmt.Errorf("expected replication.factor of %d, but got %d", expectedRF, actual)
+	}
+
+	if actual := topic.Partitions; actual != expectedPartitions {
+		return fmt.Errorf("expected %d partitions, but got %d", expectedPartitions, actual)
+	}
+
+	return nil
+}
+
+func testResourceTopic_checkSameMessages(producedMessages []*sarama.ProducerMessage) r.TestCheckFunc {
+	return func(s *terraform.State) error {
+		resourceState := s.Modules[0].Resources["kafka_topic.test"]
+		instanceState := resourceState.Primary
+		client := testProvider.Meta().(*LazyClient)
+		topicName := instanceState.Attributes["name"]
+
+		consumer, err := sarama.NewConsumerFromClient(client.inner.client)
+		if err != nil {
+			return err
+		}
+
+		defer func() {
+			if err := consumer.Close(); err != nil {
+				log.Fatalln(err)
+			}
+		}()
+
+		// all messages are produced to the same partition
+		partition := producedMessages[0].Partition
+		maxCount := len(producedMessages)
+		consumedMessages, err := testConsumePartition(consumer, topicName, partition, maxCount)
+		if err != nil {
+			return err
+		}
+
+		return testSameMessages(producedMessages, consumedMessages)
+	}
+}
+
+func testConsumePartition(consumer sarama.Consumer, topic string, partition int32, maxCount int) ([]*sarama.ConsumerMessage, error) {
+	consumedMessages := make([]*sarama.ConsumerMessage, 0, maxCount)
+
+	partitionConsumer, err := consumer.ConsumePartition(topic, partition, sarama.OffsetOldest)
+	if err != nil {
+		return consumedMessages, err
+	}
+
+	defer func() {
+		if err := partitionConsumer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	for len(consumedMessages) < maxCount {
+		select {
+		case message := <-partitionConsumer.Messages():
+			consumedMessages = append(consumedMessages, message)
+		case <- time.After(10 * time.Second):
+			return consumedMessages, nil
+		}
+	}
+
+	return consumedMessages, nil
+}
+
+func testSameMessages(produced []*sarama.ProducerMessage, consumed []*sarama.ConsumerMessage) error {
+	plen := len(produced)
+	clen := len(consumed)
+	if plen != clen {
+		return fmt.Errorf("produced and consumed message counts differ, %d != %d", plen, clen)
+	}
+
+	for i, pm := range produced {
+		cm := consumed[i]
+		if err := testMessageEqual(pm, cm); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func testMessageEqual(pm *sarama.ProducerMessage, cm *sarama.ConsumerMessage) error {
+	if pm.Partition != cm.Partition {
+		return fmt.Errorf("different partitions: %d != %d", pm.Partition, cm.Partition)
+	}
+
+	if pm.Offset != cm.Offset {
+		return fmt.Errorf("different offsets: %d != %d", pm.Offset, cm.Offset)
+	}
+
+	pmBytes, err := pm.Value.Encode()
+	if err != nil {
+		return err
+	}
+
+	pmValue := string(pmBytes)
+	cmValue := string(cm.Value)
+	if pmValue != cmValue {
+		return fmt.Errorf("different values: %v != %v", pmValue, cmValue)
+	}
+
 	return nil
 }
 
@@ -285,6 +510,24 @@ resource "kafka_topic" "test" {
   config = {
     "retention.ms" = "11111"
     "segment.ms" = "33333"
+  }
+}
+`
+
+//lintignore:AT004
+const testResourceTopic_updateRF = `
+provider "kafka" {
+  bootstrap_servers = ["%s"]
+}
+
+resource "kafka_topic" "test" {
+  name               = "%s"
+  replication_factor = %d
+  partitions         = %d
+
+  config = {
+    "retention.ms" = "11111"
+    "segment.ms" = "22222"
   }
 }
 `
