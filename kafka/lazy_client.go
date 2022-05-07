ccbc84c6edf4e65ff989dbd4e74ad8b1ec995342
Conor Mongey <conor@mongey.net>
2022-05-07 15:29:16 +0100

    Reset

--- kafka/lazy_client.go
+++ kafka/lazy_client.go
@@ -29,20 +29,16 @@ func (c *LazyClient) init() error {
 	} else {
 		log.Printf("[TRACE] lazy client init %s", c.initErr)
 	}
-
 	if c.initErr == sarama.ErrBrokerNotAvailable || c.initErr == sarama.ErrOutOfBrokers {
 		if c.Config.TLSEnabled {
 			tlsError := c.checkTLSConfig()
 			if tlsError != nil {
-				err = fmt.Errorf("%w\n%s", tlsError, c.initErr)
-
-				return nil
+				return fmt.Errorf("%w\n%s", tlsError, c.initErr)
 			}
 		}
 	}
 
 	return c.initErr
-	//return fmt.Errorf("%w\n%s", c.initErr, "kafka client is not initialized")
 }
 
 func (c *LazyClient) checkTLSConfig() error {
